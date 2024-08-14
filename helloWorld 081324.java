513c3308f4c6e0ad8661e6b137b3c7147037cb58
iiWinteredSnow <jaythanbucasas@gmail.com>
2024-08-14 10:29:26 -1000

    Update and rename helloWorld 081324 to helloWorld 081324.java

---
+++ helloWorld 081324.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+/* this code is for the first assignment. it does two simple prints and then I submit it. */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Task 1: Print out "Hello World"
+		System.out.println("Hello World!");
+		System.out.print("Hello World!");
+		// Task 2: Print out "Hello World" and then print it again on the next line
+	}
+
+}
