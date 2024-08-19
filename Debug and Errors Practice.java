a55dc1c7274819d54759515bfe47bc2957fce6b8
iiWinteredSnow <jaythanbucasas@gmail.com>
2024-08-19 12:56:47 -1000

    Create Debug and Errors Practice.java
    unfin

---
+++ Debug and Errors Practice.java
@@ -0,0 +1,37 @@
+/*
+  For each task below, explain why the code is incorrect, what type of error that is, and how to fix it.
+*/
+
+//Task 1: Print out "Hello World"
+System.out.print("Hello World);
+/* the code is incorrect because there is a missing quotation mark in ("Hello World). 
+This is a syntax error, and to fix it, you just add another quotation mark(") after the "d" in "World" 
+*/
+//Task 2: Print out "Hello World"
+System.out.print(Hello World);
+/*
+ The code is incorrect as it doesn't print out the words Hello World at all, and is missing quotations to declare the Hello World as words. 
+ To fix it, you would need to insert Hello World in between 2 quotation marks like this: ("Hello World")
+*/
+//Task 3: Print out "Hello World"
+System.Out.Print("Hello World");
+/* 
+
+*/
+//Task 4: Print out "Hello World"
+System.out.print("Hello World")
+/* 
+
+*/
+
+//Task 5: Print out "Hello World"
+System.out.print("Hello Wrold")
+/* 
+
+*/
+//Task 6: Print out "Hello World twice, one on top of the other"
+System.out.print("Hello World");
+System.out.print("Hello World");
+/* 
+
+*/
