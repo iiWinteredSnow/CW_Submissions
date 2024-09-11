2582fff05f9492f82d4ac4d13f923931b39b640e
iiWinteredSnow <jaythanbucasas@gmail.com>
2024-09-11 10:03:25 -1000

    Create methodsIntro.java

---
+++ methodsIntro.java
@@ -0,0 +1,8 @@
+/* 
+Part 1: Void Methods
+
+1. Create a void method called stateName() that prints out your name. Repeat this task for stateEyeColor(), stateHairStyle(), stateBestFriend(), and stateFavoriteFood(). Then, call stateName(), stateHairStyle, and stateFavoriteFood().
+
+2. Create a method printCircleStats() that is passed a double (meant to represent a circle's radius) and calculates then prints out the radius, diameter, circumference, and area of the circle. Call the method to make sure it works. 
+
+3. Create a method printMathStuffs() that is passed two ints and prints their sum, difference, product, quotient, and the remainder (using modulus) of the first number modulus the second number. Call it to make sure it works. */
