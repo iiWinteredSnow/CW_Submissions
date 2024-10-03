007a6dea52e531e1cd0e014c4fc81b519656b94a
iiWinteredSnow <jaythanbucasas@gmail.com>
2024-10-02 20:48:47 -1000

    Update StringPracticeOne.java

--- CodingBatSolns/StringPracticeOne.java
+++ CodingBatSolns/StringPracticeOne.java
@@ -1,1 +1,14 @@
-a
+public String helloName(String name) {
+  return "Hello " + name+"!";
+}
+public String makeAbba(String a, String b) {
+  return a+b+b+a;
+}
+public String makeTags(String tag, String word) {
+  return "<"+tag+">"+word+"</"+tag+">";
+}
+public String firstHalf(String str) {
+  int len = str.length();
+  String half = new String(str.substring(0,len/2));
+  return half;
+}
