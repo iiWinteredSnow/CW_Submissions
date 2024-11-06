2a0aafd893a8091d416b34369fdf48ab73bcf390
iiWinteredSnow <jaythanbucasas@gmail.com>
2024-11-05 22:35:10 -1000

    Update StringTwo.java

--- CodingBatSolns/StringTwo.java
+++ CodingBatSolns/StringTwo.java
@@ -1,1 +1,20 @@
-
+ public String makeOutWord(String out, String word) {
+  return out.substring(0, 2) + word + out.substring(2);
+}
+public String firstTwo(String str) {
+    // Check the string length with an if-else condition
+    if (str.length() < 2) {
+        return str;
+    } 
+     return str.substring(0, 2);
+}
+public String extraEnd(String str) {
+    String lastTwo = str.substring(str.length() - 2);
+    return lastTwo+lastTwo+lastTwo;
+}
+public String withoutEnd(String str) {
+    if (str.length() > 2) {
+        return str.substring(1, str.length() - 1);
+    }
+    return "";
+}
