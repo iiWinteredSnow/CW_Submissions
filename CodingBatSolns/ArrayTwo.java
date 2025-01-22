051c73152d8cf5553049a690e3c47a2503d9c79e
iiWinteredSnow <jaythanbucasas@gmail.com>
2025-01-22 10:03:14 -1000

    Update ArrayTwo.java

--- CodingBatSolns/ArrayTwo.java
+++ CodingBatSolns/ArrayTwo.java
@@ -6,3 +6,12 @@ public int[] rotateLeft3(int[] nums) {
 		nums[1] = temp2;
 		return nums;
 }
+
+public int[] reverse3(int[] nums) {
+		int temp1 = nums[2];
+		int temp2 = nums[1];
+		nums[2] = nums[0];
+		nums[0] = temp1;
+		nums[1] = temp2;
+		return nums;
+}
