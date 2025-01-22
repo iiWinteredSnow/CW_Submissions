00914306c056dc2c60a711d775d9b58e1a249a46
iiWinteredSnow <jaythanbucasas@gmail.com>
2025-01-22 10:21:46 -1000

    Update ArrayTwo.java

--- CodingBatSolns/ArrayTwo.java
+++ CodingBatSolns/ArrayTwo.java
@@ -27,3 +27,22 @@ public int[] maxEnd3(int[] nums) {
   return nums;
 }
 
+public int sum2(int[] nums) {
+  if (nums.length == 0) {
+        return 0;
+    } else if (nums.length == 1) {
+        return nums[0];
+    } else {
+        return nums[0] + nums[1];
+    }
+}
+
+public int[] middleWay(int[] a, int[] b) {
+  int[] nums = {a[1], b[1]};
+  return nums;
+}
+
+public int[] makeEnds(int[] nums) {
+  int[] newArr = {nums[0], nums[nums.length-1]};
+  return newArr;
+}
