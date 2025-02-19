59b5503cc241723ddd4e0e768dd37a3a4e8de428
iiWinteredSnow <jaythanbucasas@gmail.com>
2025-02-18 21:31:51 -1000

    Update ArrayThree.java

--- CodingBatSolns/ArrayThree.java
+++ CodingBatSolns/ArrayThree.java
@@ -42,3 +42,15 @@ public boolean double23(int[] nums) {
 }
 
 
+public int[] fix23(int[] nums) {
+  for (int i = 0; i<nums.length; i++){
+    if(nums[i]==2&&nums[i+1]==3){
+      nums[i+1]=0;
+      return nums;
+    }
+  }
+  return nums;
+}
+
+
+
