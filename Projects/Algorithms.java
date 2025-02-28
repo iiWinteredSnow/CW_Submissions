1e91bcdf79ef3a3f024f60e42b924a7d4f2a2782
iiWinteredSnow <jaythanbucasas@gmail.com>
2025-02-27 21:17:25 -1000

    Update Algorithms.java

--- Projects/Algorithms.java
+++ Projects/Algorithms.java
@@ -7,3 +7,33 @@
         }
         return snum;
     }
+
+  public static int indexOfSmallest(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Array must not be null or empty");
+        }
+
+        int sindex = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[sindex]) {
+                sindex = i;
+            }
+        }
+
+        return sindex;
+    }
+
+public static double average(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Array must not be null or empty");
+        }
+
+        int sum = 0;
+        for (int number : nums) {
+            sum += number;
+        }
+
+        return (double) sum / nums.length;
+    }
+
